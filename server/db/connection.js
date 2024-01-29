49da5d535b8750b5e706fdfb126d3a6a5d3a1738
faizack <faizack619@gmail.com>
2024-01-30 02:40:19 +0530

    rename db

--- server/db/connection.js
+++ server/db/connection.js
@@ -5,7 +5,7 @@ let db = null
 const connectDB = async (done) => {
     try {
         var data = await MongoClient.connect(process.env.MONGO_URL, { useNewUrlParser: true })
-        db = data.db('TestAssit')
+        db = data.db('DS_Assist')
         done()
     } catch (err) {
         done(err)
